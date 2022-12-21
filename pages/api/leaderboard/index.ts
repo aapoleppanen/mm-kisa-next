8dd2f6f7e470c9567ead2d297b9b1fcf48159eb5
Aapo Leppänen <aapo.leppanen@gmail.com>
2022-12-21 23:08:17 +0200

    feat: added points for winners and top scorers

--- pages/api/leaderboard/index.ts
+++ pages/api/leaderboard/index.ts
@@ -10,26 +10,32 @@ export default async function handle(req, res) {
       return res.status(401).send({ message: "Unauthorized" });
     }
 
-    const users: { total: number; name: string; id: string } =
+    const users: { total: number; name: string; id: string }[] =
       await prisma.$queryRaw`
-      SELECT SUM(odds) AS total, name, User.id AS id
-        FROM (
-          SELECT homeWinOdds AS Odds, id, result
-          FROM \`Match\`
-          WHERE \`Match\`.result = "HOME_TEAM"
-        UNION
-          SELECT awayWinOdds AS Odds, id, result
-          FROM \`Match\`
-          WHERE \`Match\`.result = "AWAY_TEAM"
-        UNION
-          SELECT drawOdds AS Odds, id, result
-          FROM \`Match\`
-          WHERE \`Match\`.result = "DRAW") AS od
-      INNER JOIN Pick ON od.id = Pick.matchId AND od.result = Pick.pickedResult
-      RIGHT JOIN User on User.id = Pick.userId
-      GROUP BY User.id
-      ORDER BY total DESC
-      `;
+  SELECT SUM(COALESCE(odds, 0) + COALESCE(winningOdds, 0) + COALESCE(playerOdds, 0)) AS total, User.name as name, User.id AS id, Team.winningOdds, player.playerOdds
+    FROM (
+      SELECT homeWinOdds AS Odds, id, result
+      FROM \`Match\`
+      WHERE \`Match\`.result = "HOME_TEAM"
+    UNION
+      SELECT awayWinOdds AS Odds, id, result
+      FROM \`Match\`
+      WHERE \`Match\`.result = "AWAY_TEAM"
+    UNION
+      SELECT drawOdds AS Odds, id, result
+      FROM \`Match\`
+      WHERE \`Match\`.result = "DRAW") AS od
+  INNER JOIN Pick ON od.id = Pick.matchId AND od.result = Pick.pickedResult
+  RIGHT JOIN User on User.id = Pick.userId
+  LEFT JOIN Team on Team.id = User.teamId AND Team.id = 4
+  LEFT JOIN (
+    SELECT id, odds as playerOdds, name
+    FROM Player
+    WHERE Player.id = 95
+  ) as player on player.id = User.playerId AND player.id = 95
+  GROUP BY User.id
+  ORDER BY total DESC
+  `;
 
     return res.json({
       users: JSON.parse(JSON.stringify(users)),
